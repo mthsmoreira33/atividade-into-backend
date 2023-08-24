ea11aa2f45ef89a573b29abcc5a3ed61cfb31eb6
Matheus Moreira <mthsmoreira59@gmail.com>
2023-08-23 22:19:12 -0300

    feature: adiciona cadastro de usuários

--- src/index.js
+++ src/index.js
@@ -1,11 +1,31 @@
 import express from 'express';
+import bcrypt from 'bcrypt';
 
 const app = express();
 const port = 8080;
 
+const users = [];
+
 app.use(express.json());
 
 app.get('/', (_, res) => res.status(200).json({ message: 'Rota da Aplicação.'}));
+
+//cadastro de usuários
+app.post('/user', async (req, res) => {
+    const { name, email, password } = req.body;
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    const user = {
+        id: users.length + 1,
+        name,
+        email,
+        password: hashedPassword
+    }
+
+    users.push(user);
+
+    return res.status(201).json({ message: 'Usuário criado.' });
+})
 
 app.listen(port, () => {
     console.log(`Servidor rodando na porta ${port}`);
